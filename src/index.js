71d3de82b81d7e122cf610ebff49d65ae4782500
Vishwajeet Mandal <vishmandal2003@gmail.com>
2024-10-02 01:45:20 +0530

    changes to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require(`dotenv`).config({path : `./env`})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: `./env`
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error
+        })
+    } catch(error) {
+        console.error("Error: ",error)
+        throw err
+    }
+})()
+*/
